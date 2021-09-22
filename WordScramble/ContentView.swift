25b04cd559b2d26bc0c4af350c21f455067a8ae3
Nicholas Verrico <nicholas.verrico@icloud.com>
2021-09-22 13:28:57 -0400

    closes #1, closes #3 Score now tracked
    Added score at bottom text fields need UI Enhancements. Will add new issues for that.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -17,7 +17,8 @@ struct ContentView: View {
     @State private var showingError = false
     @State private var errorTitle = ""
     @State private var errorMessage = ""
-    
+    @State private var totalScore = 0
+    @State private var wordScore = 0
     
     var body: some View {
         NavigationView {
@@ -30,9 +31,12 @@ struct ContentView: View {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
+                Text("Current Word Score: \(wordScore)")
+                Text("Total Score \(totalScore)")
+                
             }
             .navigationBarTitle(rootword)
-            .navigationBarItems(trailing: Button("New Word", action: newRound))
+            .navigationBarItems(leading: Button("Reset Score", action: newRound), trailing: Button("New Word", action: newRootWord))
             .onAppear(perform: startGame)
             .alert(isPresented: $showingError){
                 Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
@@ -53,8 +57,15 @@ struct ContentView: View {
            return wordError(title: "Word not possible", message: "That isn't a real word.")
         }
         
+        guard isLongEnoughWord(answer) else {
+            return wordError(title: "Word is too short", message: "Minimum length of 3 letters is required.")
+        }
+        
         usedWords.insert(answer, at: 0)
+        wordScore += usedWords[0].count
+        totalScore += usedWords[0].count
         newWord = emptyString
+        
     }
     
     func startGame(){
@@ -62,6 +73,7 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootword = allWords.randomElement() ?? "silkworm"
+                usedWords = []
                 return
             }
         }
@@ -74,6 +86,7 @@ struct ContentView: View {
     
     func isPossibleWord(_ word: String) -> Bool {
         var tempWord = rootword
+        
         for letter in word {
             if let pos = tempWord.firstIndex(of: letter){
                 tempWord.remove(at: pos)
@@ -86,6 +99,11 @@ struct ContentView: View {
         // Should not execute
         assertionFailure("This code should not execute, necssary for compiler safety")
         return false
+    }
+    
+    func isLongEnoughWord(_ word:String)-> Bool{
+        guard word.count >= 3 else {return false}
+        return true
     }
     
     func isRealWord(_ word: String) -> Bool {
@@ -101,8 +119,15 @@ struct ContentView: View {
         showingError = true
     }
     
+    func newRootWord(){
+        startGame()
+        wordScore = 0
+    }
+    
     func newRound(){
         startGame()
+        wordScore = 0
+        totalScore = 0
     }
     
     
