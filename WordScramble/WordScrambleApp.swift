77251ecf78c71975f00a9bcec99de70dd09f2cdf
Nicholas Verrico <nicholas.verrico@icloud.com>
2021-09-22 11:31:56 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nicholas Verrico on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
